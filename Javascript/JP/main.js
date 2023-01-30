2250f228ce3fd2a5425aae6b9a85c3a1703c0ecf
Jayy2023 <119895456+Jayy2023@users.noreply.github.com>
2023-01-30 16:15:00 -0500

    adding practice on querySelector

--- Javascript/JP/main.js
+++ Javascript/JP/main.js
@@ -1,32 +1,6 @@
-function sum(num1, num2){
-    return num1 + num2;
-}
-let sum2 = (num1, num2) =>  num1 + num2;
+document.addEventListener("DOMContentLoaded", ()=> {
+    document.querySelector("select").onchange = function(){
+        document.querySelector('#hello').style.color = this.value;
 
-function isPositive(number){
-    return number >= 0;
-}
-
-
-let isPositive2 = number => number >= 0;
- 
-
-
-
-
-function randomNumber(){
-    return Math.random();
-}
-
-let  randomNumber = () => Math.random();
-
-
-
-
-document.addEventListener("click", function(){
-    console.log("click");
+    }
 })
-
-
-  document.addEventListener("click",  () => console.log("click"));
-    
